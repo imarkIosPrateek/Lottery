9357054cf4f2a387c6d9541eb6d81d022d4d0219
iMark <imark@iMarks-Mac-mini.local>
2018-02-02 15:39:40 +0530

    push code into server

---
+++ Lottery/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Lottery
+//
+//  Created by iMark on 02/02/18.
+//  Copyright © 2018 iMark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+       
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
